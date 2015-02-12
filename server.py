bacd992b15a03ea158979a657bd22143851cd6cb
Fengda HUANG <h@phodal.com>
2015-02-12 19:57:10 +0800

    Adjust js directory

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext import restful
 from flask_restful import Resource
+import requests
 
 app = Flask(__name__, static_url_path='')
 app.secret_key = 'why would I tell you my secret key?'
@@ -11,7 +12,10 @@ api = restful.Api(app)
 class All(Resource):
     @staticmethod
     def get():
-        return "", 201, {'Access-Control-Allow-Origin': '*'}
+        base_url = ''
+        query = 'geo%3A%22IsWithin(POLYGON((9.167648038864687+45.47779519240331%2C9.2129666423803+45.44432337237933%2C9.216399869919362+45.48501671903388%2C9.167648038864687+45.47779519240331)))+distErrPct%3D0%22+&wt=json&indent=true'
+        result = requests.get((base_url + ('select?q=' + query)))
+        return result.json(), 201, {'Access-Control-Allow-Origin': '*'}
 
 
 api.add_resource(All, '/geo/')

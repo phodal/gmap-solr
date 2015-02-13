991a67bd885d4ddca491520efacb6c76f56e5c97
Fengda HUANG <h@phodal.com>
2015-02-13 20:46:01 +0800

    Add parse ip

--- parseGeo.py
+++ parseGeo.py
@@ -2,10 +2,11 @@
 # coding=utf-8
 
 import json
+import urllib
 
 with open(unicode('data/report.json')) as data_file:
-    print data_file
     data = json.load(data_file)
 
 for ip in data['hosts']:
-    print ip['data']
+    response = urllib.urlopen('http://api.hostip.info/get_html.php?ip=' + ip['data'] + '&position=true').read()
+    print response

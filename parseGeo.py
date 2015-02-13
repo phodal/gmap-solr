1f1cb0e0331ef06a9c0196e411a65238c7b6c901
Fengda HUANG <h@phodal.com>
2015-02-13 20:40:16 +0800

    parse ip from data

--- parseGeo.py
+++ parseGeo.py
@@ -2,9 +2,10 @@
 # coding=utf-8
 
 import json
-from pprint import pprint
 
 with open(unicode('data/report.json')) as data_file:
     print data_file
     data = json.load(data_file)
-pprint(data)
+
+for ip in data['hosts']:
+    print ip['data']
